379d5aa8cc18a662514f38f6fb4ea412634c691d
jack jie <461557724@qq.com>
2019-04-12 19:14:38 +0800

    ajax登录校验完成

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -21,6 +21,9 @@ $(function(){
             min: 2,
             max: 6,
             message: "用户名长度必须2-6位"
+          },
+          callback: {
+            message: "用户名不存在"
           }
         }
       },
@@ -34,9 +37,43 @@ $(function(){
             min: 6,
             max: 12,
             message: "密码长度必须6-12位"
+          },
+          callback: {
+            message: "密码错误"
           }
         }
       }
     }
   })
+
+  $("#form").on("success.form.bv",function(e){
+    e.preventDefault();
+
+    console.log("校验成功后 表单提交组织了");
+    $.ajax({
+      type: "post",
+      url: "/employee/employeeLogin",
+      data: $("#form").serialize(),
+      dataType: "json",
+      success: function (info ) {
+        console.log(info);
+        if (info.success) {
+          location.href = "index.html";
+        }
+        if (info.error === 1000) {
+          $("#form").data("bootstrapValidator").updateStatus("username", "INVALID", "callback")
+        }
+        if (info.error === 1001) {
+          $("#form").data("bootstrapValidator").updateStatus("password", "INVALID", "callback")
+        }
+      }
+    })
+  })
+
+  // 3 重置功能
+  $("[type='reset']").click(function(){
+    $("#form").data("bootstrapValidator").resetForm(true);
+  })
+
+
 })
